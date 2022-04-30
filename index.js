02328b4a7ee046798fa5d544f93375885291168b
romeodesa <hromeodesa7mol@gmail.com>
2022-04-30 16:57:48 -0600

    prueba mb2

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Prueba Montana Bistro");
+  //res.send("Prueba Montana Bistro");
+  res.sendFile("./public/index.html");
 });
 
 app.listen(process.env.PORT || 5000);
