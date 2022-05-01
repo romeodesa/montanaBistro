cd5f207dcf3ec66892a2e2dd3f1cf4ca8b997663
romeodesa <hromeodesa7mol@gmail.com>
2022-04-30 18:53:15 -0600

    prueba mb4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", function (req, res) {
   //res.send("Prueba Montana Bistro");
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
