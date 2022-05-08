3ae5287d9b6679c58217963c3b8b7b79fe45ee8b
romeodesa <hromeodesa7mol@gmail.com>
2022-05-08 10:02:00 -0600

    vista general 4

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-  //console.log("Application started and Listening on port 3000");
-});
+app.listen(process.env.PORT || 5000);
 
 app.use(express.static(__dirname));
 app.use(express.static('css'));
